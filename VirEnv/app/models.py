ba630aca4681c14ca6402d22a8b4a042da092212
Arran <tartankavujr@gmail.com>
2019-10-03 00:44:06 +0100

    second up date relationship now work properly

--- VirEnv/app/models.py
+++ VirEnv/app/models.py
@@ -1,20 +1,29 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-
-class User(db.Model):
+class User(Usermixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64),index=True, unique=True)
-    email = db.Column(db.String(120),index=True,unique=True)
+    email = db.Column(db.String(120),index=True ,unique=True)
     pwd_hash = db.Column(db.String(256))
     posts =db.relationship('Posts',backref='author',lazy='dynamic')
                          
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.pwd_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.pwd_hash , password)
+
+
 class Posts(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
+    timestamp = db.Column(db.DateTime,index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
 
     def __repr__(self):
