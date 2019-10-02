95fdf8c7ee4ce56c1a0dec553e5627ec9d7eda0c
Arran <tartankavujr@gmail.com>
2019-10-02 23:50:09 +0100

    DB relationships #WE
    relationships do not work so far

--- VirEnv/Config.py
+++ VirEnv/Config.py
@@ -6,5 +6,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,'app.db')
     print (SQLALCHEMY_DATABASE_URI)
-    DATABASE_URL = 'sqlite///' + os.path.join(basedir,'app.db')
+    DATABASE_URL = 'sqlite///:' + os.path.join(basedir,'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
