c9f2d977e5631640f7688c86344480c414316590
Arran <tartankavujr@gmail.com>
2019-10-12 14:06:12 +0100

    login and user registration

--- VirEnv/app/routes.py
+++ VirEnv/app/routes.py
@@ -1,30 +1,39 @@
 from flask import render_template, flash, redirect, url_for, request
-from app import app
+from app import app, db
 from flask_login import current_user , login_user, logout_user , login_required
-from app.models import User
+from app.models import User, Posts
 from werkzeug.urls import url_parse
 
-from app.form import LoginForm
+from app.form import LoginForm , RegistrationForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-   
-    user = {'username': 'Arran'}
+    i=0
+    """
+    for post in Posts.query.get():
+        posts[i]= {
+            'author': post.author,
+             'body': post.body,
+             'timestamp': post.timestamp
+             }
+        i = i +1
+    """
     posts = [
         {
-            'author': {'username':'John'},
-            'body': 'What a Post'
+            'author':'Arran',
+            'body':'fake post lol'
         },
         {
-            'author': {'username': 'Tyler'},
-            'body' : 'This website is growing!'
+            'author':'Carter',
+            'body':'why are you including me'
         }
     ]
-    print("index for : " + user['username'])
-    return render_template('index.html', title='Home',user=user, posts=posts)
+    if current_user.is_authenticated:
+        print("index for : " + current_user.username)
 
+    return render_template('index.html', title='Home', posts=posts)
 
 @app.route('/login', methods=['GET' , 'POST'])
 def Login():
@@ -59,3 +68,19 @@ def userProfile():
 def Logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+
+@app.route('/Register', methods=['GET','POST'])
+def Register():
+    if current_user.is_authenticated:
+        return redirect (url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data,email = form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulation, you are a new registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title="Register Now", form=form)
